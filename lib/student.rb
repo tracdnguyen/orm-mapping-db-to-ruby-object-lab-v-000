13f9e35f9d4a25f4a21c64a45fb376853b41228b
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-08 16:07:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -114,7 +114,7 @@ class Student
     end.first
   end
 
-  def self.all_students_in_grade_X(students)
+  def self.all_students_in_grade_X(grade)
     sql = <<-SQL
       SELECT *
       FROM students

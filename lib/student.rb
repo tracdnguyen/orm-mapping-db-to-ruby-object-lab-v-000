a652acc789f1ca2ed5e9e83b54f1fe4e82930446
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-08 15:49:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,6 +65,14 @@ class Student
   end
 
   def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+    SQL
 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 end

12bfe4ee82dd2935a295dc22b8a4e73b68497b90
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-08 16:07:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -113,4 +113,17 @@ class Student
       self.new_from_db(row)
     end.first
   end
+
+  def self.all_students_in_grade_X(students)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
 end

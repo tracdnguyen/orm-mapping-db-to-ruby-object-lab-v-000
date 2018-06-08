cddbe0e4bd984998c91e7a3bbb9a9fda8bce5bd0
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-08 15:55:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -100,4 +100,14 @@ class Student
       self.new_from_db(row)
     end
   end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+
+  end
 end
